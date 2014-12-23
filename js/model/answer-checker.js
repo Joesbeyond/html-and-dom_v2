fddd13ab5f09fb94b6451674a7c7bd3ce735c3f7
joesbeyond <joesbeyond@gmail.com>
2014-12-23 23:02:11 +0800

    forget details

---
+++ js/model/answer-checker.js
@@ -0,0 +1,20 @@
+var _ = require("../lib/lodash-2.4.1/lodash");
+function AnswerChecker(document, topics) {
+    this.document = document;
+    this.topics = topics;
+}
+
+AnswerChecker.prototype.getTotalScore = function () {
+
+    var _this = this;
+
+    _.each(this.topics, function (topic) {
+        topic.calculate(_this.document);
+    });
+
+    var scores = _.pluck(_this.topics, 'score');
+    return _.reduce(scores, function (scoreA, scoreB) {
+        return scoreA + scoreB;
+    });
+};
+module.export = AnswerChecker;
---
+++ js/model/topic.js
@@ -0,0 +1,11 @@
+function Topic(name, answer, scoreUnit) {
+    this.name = name;
+    this.answer = answer;
+    this.scoreUnit = scoreUnit;
+    this.score = 0;
+}
+
+Topic.prototype.calculate = function (document) {
+    // need to overriding this function.
+};
+module.export = Topic;
